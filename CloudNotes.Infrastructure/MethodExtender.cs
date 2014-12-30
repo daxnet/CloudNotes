c3a3f98c764abebdf29b75506d231023cce94948
daxnet <daxnet@outlook.com>
2014-12-30 16:51:00 +0800

    Added the RemoveHtmlTags implementation.

--- CloudNotes.Infrastructure/MethodExtender.cs
+++ CloudNotes.Infrastructure/MethodExtender.cs
@@ -3,6 +3,8 @@ using System;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Diagnostics;
+using System.Text;
+using System.Web;
 
 namespace CloudNotes.Infrastructure
 {
@@ -11,6 +13,11 @@ namespace CloudNotes.Infrastructure
     /// </summary>
     public static class MethodExtender
     {
+        #region Private Static Fields
+        private static readonly Regex Tags = new Regex(@"<[^>]+?>", RegexOptions.Multiline | RegexOptions.Compiled);
+        private static readonly Regex NotOkCharacter = new Regex(@"[^\w;&#@.:/\\?=|%!() -]", RegexOptions.Compiled);
+        #endregion
+
         #region String Extender
         // ReSharper disable InconsistentNaming
         /// <summary>
@@ -22,7 +29,7 @@ namespace CloudNotes.Infrastructure
         /// <param name="mapping">The mapping.</param>
         /// <returns></returns>
         public static PagedResult<U> CastPagedResult<T, U>(this PagedResult<T> source, Func<T, U> mapping)
-            // ReSharper restore InconsistentNaming
+        // ReSharper restore InconsistentNaming
         {
             return new PagedResult<U>(source.TotalRecords,
                 source.TotalPages, source.PageSize, source.PageNumber,
@@ -41,12 +48,84 @@ namespace CloudNotes.Infrastructure
 
         }
 
+        /// <summary>
+        /// Opens the web browser and navigates to the given url.
+        /// </summary>
+        /// <param name="s">The web address on which the navigator should open.</param>
         public static void Navigate(this string s)
         {
             if (!string.IsNullOrEmpty(s))
             {
                 Process.Start(s);
             }
+        }
+
+        /// <summary>
+        /// Removes all the HTML tags and bad characters from the given HTML string.
+        /// </summary>
+        /// <param name="html">The source HTML string.</param>
+        /// <returns></returns>
+        public static string RemoveHtmlTags(this string html)
+        {
+            html = HttpUtility.UrlDecode(html);
+            html = HttpUtility.HtmlDecode(html);
+
+            html = RemoveTag(html, "<!--", "-->");
+            html = RemoveTag(html, "<script", "</script>");
+            html = RemoveTag(html, "<style", "</style>");
+
+            //replace matches of these regexes with space
+            html = Tags.Replace(html, " ");
+            html = NotOkCharacter.Replace(html, " ");
+            html = SingleSpacedTrim(html);
+
+            return html;
+        }
+        #endregion
+
+        #region Private Static Methods
+        private static String RemoveTag(String html, String startTag, String endTag)
+        {
+            bool bAgain;
+            do
+            {
+                bAgain = false;
+                var startTagPos = html.IndexOf(startTag, 0, StringComparison.CurrentCultureIgnoreCase);
+                if (startTagPos < 0) continue;
+                var endTagPos = html.IndexOf(endTag, startTagPos + 1, StringComparison.CurrentCultureIgnoreCase);
+                if (endTagPos <= startTagPos) continue;
+                html = html.Remove(startTagPos, endTagPos - startTagPos + endTag.Length);
+                bAgain = true;
+            }
+            while (bAgain);
+            return html;
+        }
+
+        private static String SingleSpacedTrim(String inString)
+        {
+            var sb = new StringBuilder();
+            var inBlanks = false;
+            foreach (var c in inString)
+            {
+                switch (c)
+                {
+                    case '\r':
+                    case '\n':
+                    case '\t':
+                    case ' ':
+                        if (!inBlanks)
+                        {
+                            inBlanks = true;
+                            sb.Append(' ');
+                        }
+                        continue;
+                    default:
+                        inBlanks = false;
+                        sb.Append(c);
+                        break;
+                }
+            }
+            return sb.ToString().Trim();
         }
         #endregion
     }
